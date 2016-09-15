8e79f0aca4575b76bdac4a52fec689a7cd2d26cf
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-09-15 09:56:40 -0400

    first commit

---
+++ Problem4Slide/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4Slide
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            int[] example = { 20, 10, 5, 2 };
+            Console.WriteLine("\n" + "The sum of the example array is {0}", Sum(example));
+
+        }
+        static int Sum(int[] num)
+        {
+            int answer = 0;
+            foreach (int x in num)
+            {
+                answer += x;
+            }
+            return answer;
+            
+            
+
+        }
+
+    }
+}
